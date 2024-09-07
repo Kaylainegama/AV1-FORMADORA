bc9cf698f69a77767936ca5da519e2b276ec5632
kaylaine <kaydsg01@gmail.com>
2024-09-07 20:15:39 -0300

    Initial commit

---
+++ AV1FORMADORA/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AV1FORMADORA',
+  webDir: 'www'
+};
+
+export default config;
